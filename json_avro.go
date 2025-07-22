2a5b2827b4b883333073932d9d6b434cf59b2ef6
karthick <a.karthie+git@pm.me>
2025-07-22 21:49:54 +0100

    chore(refactor): move all files to root

---
+++ json_avro.go
@@ -0,0 +1,11 @@
+package json2avro
+
+func TransferAvroToJson(avroData []byte) error {
+	jsw, err := newJsonWrapper(avroData)
+	if err != nil {
+		return err
+	}
+	assignProperties(jsw.JS, jsw.AS)
+	jsonAvroDataTypeMap(jsw)
+	return nil
+}
